f5c3eafc83f03f8aec311db442b180f1be9d8df4
Sineway <sineway@yandex.com>
2020-04-15 10:26:22 +0300

    Update exports

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-export {GetSet} from "./lib/get-set.js";
-export {constant, type, defaultValueSymbol} from "./lib/utilities.js";
+export * from "./lib/get-set.js";
+export * from "./lib/get-set-entry.js";
+export * from "./lib/get-set-handler.js";
+export * from "./lib/utilities.js";
